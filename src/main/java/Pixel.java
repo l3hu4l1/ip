8ffef3aff3cd03188177c6539fd3f0b0756dff56
Le Hua <l3hu4l1@gmail.com>
2026-01-23 14:39:09 +0800

    replace all mentions of "Duke" to "Pixel"

---
+++ src/main/java/Pixel.java
@@ -0,0 +1,10 @@
+public class Pixel {
+    public static void main(String[] args) {
+        String logo = " ____  _          _  \n"
+                + "|  _ \\(_)_  _____| | \n"
+                + "| |_) | \\ \\/ / _ \\ | \n"
+                + "|  __/| |>  <  __/ | \n"
+                + "|_|   |_/_/\\_\\___|_| \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
