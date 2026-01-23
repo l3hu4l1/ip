5506a49d3f19659c076af301aad8a9fc9427e0c9
Le Hua <l3hu4l1@gmail.com>
2026-01-23 15:05:43 +0800

    add support for list

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -8,6 +8,8 @@ public class Pixel {
                 + "|  __/| |>  <  __/ | \n"
                 + "|_|   |_/_/\\_\\___|_| \n";
         String line = "____________________________________________________________";
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         System.out.println(line);
         System.out.println(" Hello! I'm\n" + logo);
@@ -20,12 +22,31 @@ public class Pixel {
         while (true) {
             input = scanner.nextLine();
             System.out.println(line);
-            System.out.println(" " + input);
-            System.out.println(line);
 
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
+                }
+            } else if (input.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[taskIndex]);
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                tasks[taskIndex].markAsNotDone();
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[taskIndex]);
+            } else {
+                tasks[taskCount] = new Task(input);
+                taskCount++;
+                System.out.println(" added: " + input);
             }
+
+            System.out.println(line);
         }
 
         scanner.close();
