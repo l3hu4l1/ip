99d393c78244dca96881a4c30ab3ff7192c5a3f2
Le Hua <l3hu4l1@gmail.com>
2026-01-30 21:05:04 +0800

    Add Gradle

---
+++ src/test/java/pixel/PixelTest.java
@@ -0,0 +1,13 @@
+package pixel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PixelTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+}
