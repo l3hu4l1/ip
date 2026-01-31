0a533f7551175c102055e65b2079d674ca6283e1
Le Hua <l3hu4l1@gmail.com>
2026-01-31 15:09:34 +0800

    Add javadoc

--- src/main/java/pixel/task/Task.java
+++ src/main/java/pixel/task/Task.java
@@ -1,15 +1,27 @@
 package pixel.task;
 
+/**
+ * Abstract base class representing a generic task.
+ * Contains common properties and behavior shared by all task types.
+ */
 public abstract class Task {
     protected String description;
     protected TaskStatus status;
 
+    /**
+     * Creates a new Task with the specified description.
+     *
+     * @param description The description of the task
+     */
     public Task(String description) {
         this.description = description;
         this.status = TaskStatus.NOT_DONE;
     }
-
-    public String getStatusIcon() {
+    /**
+     * Gets the status icon representing whether the task is done.
+     *
+     * @return "X" if task is done, " " otherwise
+     */    public String getStatusIcon() {
         return status.getIcon();
     }
 
