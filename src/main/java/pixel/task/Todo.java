2eb1ecaa224f23dc37eda13ea4d89d0265bc63a0
Le Hua <l3hu4l1@gmail.com>
2026-01-31 15:09:59 +0800

    Add javadoc

--- src/main/java/pixel/task/Todo.java
+++ src/main/java/pixel/task/Todo.java
@@ -1,6 +1,14 @@
 package pixel.task;
 
+/**
+ * Represents a todo task without any specific time constraints.
+ */
 public class Todo extends Task {
+    /**
+     * Creates a new Todo task with the specified description.
+     *
+     * @param description The description of the todo task
+     */
     public Todo(String description) {
         super(description);
     }
