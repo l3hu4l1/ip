67c61fda441557d9e5246143f3ae94d9f8aa9c3a
Le Hua <l3hu4l1@gmail.com>
2026-02-04 10:34:43 +0800

    Change entrypoint

---
+++ src/main/java/pixel/ui/Main.java
@@ -0,0 +1,33 @@
+package pixel.ui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import pixel.Pixel;
+
+public class Main extends Application {
+
+    private final Pixel pixel = new Pixel();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setPixel(pixel);  // inject the Pixel instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
